e0cb608e2a1612590ed054fb09299aae4cab9ebd
NickLont <nlontorfos@hotmail.com>
2021-12-28 22:01:16 +0100

    CSS added

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<!-- Styling for all Routes -->
+<script>
+    import '../app.css'
+</script>
+
+<!-- We can also do things like apply html to every route here -->
+
+<main>
+    <slot />
+</main>
+<!-- <footer></footer> -->
